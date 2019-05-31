b34a4d5167f29b1d0572e2900af9fbf65223e72f
yurikrupnik <krupnik.yuri@gmail.com>
2019-05-31 05:19:18 +0300

    circle build

--- packages/app2/src/index.jsx
+++ packages/app2/src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import routes from './components/routes';
-import './styles/_index.scss';
+// import './styles/_index.scss';
 
 render(
     <BrowserRouter>
