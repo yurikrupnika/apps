80b96065083b2e544ae58a30f33a0321e0da5aa4
yurikrupnik <krupnik.yuri@gmail.com>
2019-06-27 00:53:35 +0300

    docker-compose prod updating image

--- packages/webserver1/src/config.js
+++ packages/webserver1/src/config.js
@@ -7,7 +7,7 @@ console.log('process.env.DOCKER_HOST', process.env.DOCKER_HOST); // eslint-disab
 const destHost = process.env.DESTINATION_HOST || process.env.DOCKER_HOST || 'http://localhost';
 const port = Number(process.env.PORT) || 5000;
 const isProd = process.env.NODE_ENV === 'production';
-const host = process.env.DOCKER_HOST || 'http://localhost';
+const host = process.env.HOST || 'http://localhost';
 const destPort = Number(process.env.DEST_PORT) || 3000;
 console.log('host', host); // eslint-disable-line
 console.log('destPort', destPort); // eslint-disable-line
