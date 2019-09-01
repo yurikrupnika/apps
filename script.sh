4831723d1644c07ce50580175f7fffc2d12642c5
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-02 02:10:43 +0300

    script update

--- script.sh
+++ script.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 npx lerna publish patch --yes --no-push
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-git add add ./packages/services/projects/package-lock.json
-git add add ./packages/services/service1/package-lock.json
-git add add ./packages/ui/list/package-lock.json
-git add add ./packages/ui/button/package-lock.json
-git add add ./packages/node/render/package-lock.json
-git add add ./packages/node/express-responses/package-lock.json
+git add .
+#git add add ./packages/services/projects/package-lock.json
+#git add add ./packages/services/service1/package-lock.json
+#git add add ./packages/ui/list/package-lock.json
+#git add add ./packages/ui/button/package-lock.json
+#git add add ./packages/node/render/package-lock.json
+#git add add ./packages/node/express-responses/package-lock.json
 #git commit --amend --no-edit
 #VERSION=`node -pe "require('$PWD/package.json').version;"`
 git tag -f 0.0.3
-git commit --amend --no-edit
+git commit -am "package-lock.json update"
 #npm version patch
 git push --follow-tags
 
