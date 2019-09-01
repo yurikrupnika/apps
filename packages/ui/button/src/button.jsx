76d6c6d38826eef09d8016cf345d1022e8124bc8
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-01 21:47:09 +0300

    changing list and button

--- packages/ui/button/src/button.jsx
+++ packages/ui/button/src/button.jsx
@@ -4,22 +4,6 @@ import PropTypes from 'prop-types';
 import MuButton from '@material-ui/core/Button';
 import styles from './button.scss';
 
-
-// const useStyles = makeStyles({
-//     root: {
-//         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-//         borderRadius: 3,
-//         border: 0,
-//         color: 'white',
-//         height: 48,
-//         padding: '0 30px',
-//         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//     },
-//     label: {
-//         textTransform: 'capitalize',
-//     },
-// });
-
 function Button(props) {
     const { children, onClick } = props;
     return (
