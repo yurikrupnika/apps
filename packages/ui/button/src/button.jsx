8ba39c3b36e4a4ea0480c66b321bff5b70551812
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-01 14:53:00 +0300

    changed button

--- packages/ui/button/src/button.jsx
+++ packages/ui/button/src/button.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import MuButton from '@material-ui/core/Button';
 import styles from './button.scss';
 
+
 // const useStyles = makeStyles({
 //     root: {
 //         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
