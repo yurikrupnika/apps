a5cb6ebf833b352bfacecab928b048a7ece1b6e1
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-02 01:38:36 +0300

    script update

--- script.sh
+++ script.sh
@@ -2,9 +2,11 @@
 npx lerna publish --yes --no-push
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add **/package-lock.json
+#git commit --amend --no-edit
+#VERSION=`node -pe "require('$PWD/package.json').version;"`
+#git tag -f 5.5.5
+npm version patch
 git commit --amend --no-edit
-VERSION=`node -pe "require('$PWD/package.json').version;"`
-git tag -f 5.5.5
 git push --follow-tags
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
