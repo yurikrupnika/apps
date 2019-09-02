5c37ed203db8efcc156c462cb85dd793b1e6c681
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-03 01:32:48 +0300

    a

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add .
 git commit -m "package-lock.json update"
 npm version patch
-git push --follow-tags origin
+git push origin --follow-tags
 
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
