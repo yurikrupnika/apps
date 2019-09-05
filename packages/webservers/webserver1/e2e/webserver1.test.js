5c7b96c60e05ada72d72f4d2bbb565326faa99b2
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-06 01:47:41 +0300

    testing e2e in circleci again

--- packages/webservers/webserver1/e2e/webserver1.test.js
+++ packages/webservers/webserver1/e2e/webserver1.test.js
@@ -41,7 +41,7 @@ describe('App', () => {
     test('links', async () => {
         await fs.mkdir('e2e/screens', { recursive: true }, () => {
         });
-        const response = await page.goto('0.0.0.0:9000');
+        const response = await page.goto('http://0.0.0.0:9000');
         assert(response.ok());
         // await page.goto('http://localhost:9001/');
         const hrefs = await page.$$eval('a', as => as.map(a => a.href));
