47cbc2a4791fd1bddd7705133f31eb863a8cdbe5
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-21 03:54:20 +0300

    fg

--- packages/webservers/webserver1/src/components/App/app.jsx
+++ packages/webservers/webserver1/src/components/App/app.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Providers from './providers';
 import Layout from './layout';
+import { Auth0Provider } from '../contexts/auth/auth';
+
+const onRedirectCallback = (appState) => {
+    global.window.history.replaceState(
+        {},
+        global.document.title,
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : global.window.location.pathname
+    );
+};
 
 const App = ({ routes, providers }) => (
-    <Providers providers={providers}>
-        <Layout routes={routes} />
-    </Providers>
+    <Auth0Provider
+        domain="dev-pd1t9vho.eu.auth0.com"
+        client_id="S0ySbSRlA6fftvfvtj9N6gFbvGuWEgR5"
+        audience="https://quickstarts/api"
+        redirect_uri={global.window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+    >
+        <Providers providers={providers}>
+            <Layout routes={routes} />
+        </Providers>
+    </Auth0Provider>
 );
 
 App.defaultProps = {
--- packages/webservers/webserver2/src/components/App/app.jsx
+++ packages/webservers/webserver2/src/components/App/app.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Providers from './providers';
 import Layout from './layout';
+import { Auth0Provider } from '../contexts/auth/auth';
 
-/**
- * @class App
- * @param {array} routes Application routes
- * @param {array} providers Application providers
- */
+const onRedirectCallback = (appState) => {
+    global.window.history.replaceState(
+        {},
+        global.document.title,
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : global.window.location.pathname
+    );
+};
 
 const App = ({ routes, providers }) => (
-    <Providers providers={providers}>
-        <Layout routes={routes} />
-    </Providers>
+    <Auth0Provider
+        domain="dev-pd1t9vho.eu.auth0.com"
+        client_id="V0B0FRqPAob8RGZxeIAMZ9lNw9S6tjeE"
+        audience="https://localhost:7001/api/projects"
+        redirect_uri={global.window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+    >
+        <Providers providers={providers}>
+            <Layout routes={routes} />
+        </Providers>
+    </Auth0Provider>
 );
 
 App.defaultProps = {
