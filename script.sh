6eab999b67e82027af92bbd95c734b4d8c08490a
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-24 00:18:42 +0300

    changed script

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ npx lerna publish major --yes --amend
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add .
 git commit -m "package-lock.json update"
-npm version major
+#npm version major
 git push origin --follow-tags
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
