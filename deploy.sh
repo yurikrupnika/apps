34007cadf6a5121f869240f66a55800a777eab64
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-29 15:09:54 +0300

    added to deploy and green color changed

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npx lerna publish patch --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -m "package-lock.json update"
-npm version patch
+npm version patch -m "update version"
 git push origin --follow-tags
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
