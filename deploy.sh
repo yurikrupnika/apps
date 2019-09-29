e6f1d8cbd8dd2a088cd8febb14667f6a3ade5b2a
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-29 18:17:36 +0300

    added  git push origin --tags

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -am "package-lock.json update"
 npm version patch -am "update version"
+git push origin --tags
 git tag "$(node -p "require('./package.json').version")"  -a -m "update shit"
 git push origin --tags
 
