18765c7716afbc127c62d8c4094b38e8ff2736b1
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-29 16:08:32 +0300

    g

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ npm run build
 npx lerna publish patch --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
-git commit -m "package-lock.json update"
-npm version patch -m "update version"
+git commit -am "package-lock.json update"
+npm version patch -am "update version"
 git tag "$(node -p "require('./package.json').version")"  -a -m "update shit"
-git push origin --follow-tags
+git push origin --tags
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
