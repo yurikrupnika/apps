6412fe353797734a1eb21eb296ed1cee36ea8a53
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-29 15:32:50 +0300

    deoloy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -m "package-lock.json update"
 npm version patch -m "update version"
-git tag "$(node -p "require('./package.json').version")"  -a
+git tag "$(node -p "require('./package.json').version")"  -a -m '"$(node -p "require('./package.json').version")" '
 git push origin --follow-tags
 
 # "postversion": "npm install --package-lock && git add package-lock.json && commit --amend --no-edit"
