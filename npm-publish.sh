d3be54f05d0f0a588f649ce21ce6b96f72a9b98a
yurikrupnik <krupnik.yuri@gmail.com>
2019-10-07 03:38:46 +0300

    changed script npm publish to tag with minor

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 npm run build
-npx lerna publish patch --yes --no-push --conventional-commits
+npx lerna publish minor --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -am "package-lock.json update"
-npm version patch
+npm version minor
 git push origin --follow-tags
 
 # tag
