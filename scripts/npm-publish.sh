3d0b96fc4a158daff7c720e27001e9d7f02d483c
yurikrupnik <krupnik.yuri@gmail.com>
2019-12-01 01:34:38 +0200

    since-lint_test_build-docker

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,13 +1,22 @@
 #! /bin/bash
 
+set -e
+npx lerna changed -a > 1
+if [ `npx lerna changed -a -q` ]; then
+  echo all good, can do npm publish
+else
+echo no changes to lerna changed -a
+#circleci-agent step halt
+exit 1
+fi
 npm run build
-npx lerna publish major --yes --no-push --conventional-commits
-npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-git add -u
-git commit -am "package-lock.json update"
-npm version major
-git push origin --follow-tags
-
-# tag
-git tag "$(node -p "require('./package.json').version")"  -a -m "$(node -p "require('./package.json').version")"
-git push origin --follow-tags
+#npx lerna publish major --yes --no-push --conventional-commits
+#npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+#git add -u
+#git commit -am "package-lock.json update"
+#npm version major
+#git push origin --follow-tags
+#
+## tag
+#git tag "$(node -p "require('./package.json').version")"  -a -m "$(node -p "require('./package.json').version")"
+#git push origin --follow-tags
