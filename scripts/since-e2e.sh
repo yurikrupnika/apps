e25d3bf367779fe469f5e3201fc757eb1399a63c
yurikrupnik <krupnik.yuri@gmail.com>
2019-11-30 04:25:03 +0200

    if [ `npx lerna changed` ]; then     echo all good else  echo expression evaluated as false # exit 1 fi

---
+++ scripts/since-e2e.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#docker images
+#docker-compose -f docker-compose.lerna.yml build  --force-rm
+if [ `npx lerna changed` ]; then
+    echo all good
+else
+ echo expression evaluated as false
+# exit 1
+fi
+
+# execute command
+#$@ npx lerna changed
+##docker-compose build $("npx lerna changed -q") --parallel
+#if [ $STATUS == 0 ]; then
+#  echo "Command '$@' completed successfully"
+#else
+#  echo "Command '$@' exited with error status $STATUS"
+#fi
