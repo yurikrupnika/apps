b717038666001350f671bfce1877a23ad650aef0
yurikrupnik <krupnik.yuri@gmail.com>
2019-12-14 03:15:40 +0200

    back to patch

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,7 +5,8 @@
 #echo $s
 
 npm run build
-npx lerna publish prepatch --dist-tag lol --yes --no-push --conventional-commits
+#npx lerna publish prepatch --dist-tag lol --yes --no-push --conventional-commits
+npx lerna publish path --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -am "package-lock.json update"
