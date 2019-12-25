f20af4e0a05ff6f946192b4c71b33f8d1fd88583
yurikrupnik <krupnik.yuri@gmail.com>
2019-12-26 01:06:20 +0200

    a

--- packages/webservers/webserver2/src/index.jsx
+++ packages/webservers/webserver2/src/index.jsx
@@ -4,6 +4,7 @@ import os from 'os';
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+// import cors from 'cors';
 // import axios from 'axios';
 // import render from '@krupnik/render';
 import proxy from 'express-http-proxy';
@@ -13,7 +14,24 @@ import {
 } from './config';
 import App from './components/App';
 import routes from './components/routes';
+// import axios from 'axios';
+//
+// const key = '2fbeed25338c7978968f34f367d1ca1b';
 
+// const api = () => {
+//     return axios.get('https://api.getblueshift.com/api/v1/campaigns.json?end_time=2019-10-31T18:29:59.999Z&start_time=2019-09-02T18:30:00.000Z', {
+//         headers: {
+//             Authorization: `Bearer ${key}`,
+//             'Access-Control-Allow-Origin': '*'
+//         }
+//     })
+//         .then((res) => {
+//             console.log(res.data);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// };
 const webServer = express();
 
 const assets = path.join(process.cwd(), !isProd ? 'dist' : '', 'assets');
@@ -27,9 +45,20 @@ webServer.set('view engine', 'ejs');
 webServer.set('views', assets);
 
 const route = express.Router();
+// const route1 = express.Router();
+// route1.get('/', (request, response) => {
+//     api
+//         .then((res) => {
+//             response.json(res.data);
+//         })
+//         .catch((error) => {
+//             response.json(error);
+//         });
+// });
 route.all('/api/users', proxy(`${destHost}:${destPort}`));
 
 webServer.use(route);
+// webServer.use('/buc', route1);
 webServer.use((req, res, next) => {
     console.log('host', host); // eslint-disable-line no-console
     console.log('host', os.hostname()); // eslint-disable-line no-console
