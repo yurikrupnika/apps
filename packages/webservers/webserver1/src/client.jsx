7899ce54a8e91ad5993efebb30d55b1c68e2ebcf
yurikrupnik <krupnik.yuri@gmail.com>
2019-12-26 03:37:54 +0200

    dsa

--- packages/webservers/webserver1/src/client.jsx
+++ packages/webservers/webserver1/src/client.jsx
@@ -4,8 +4,6 @@ import { render, hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import routes from './components/routes';
-// import { Provider } from './services/users/context';
-// import { Provider as ThemeProviders } from './components/contexts/themes';
 import config from './config';
 import './styles/_index.scss';
 
