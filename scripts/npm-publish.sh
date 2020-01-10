265a304010d7dc9247655db56678c01d3d667a96
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 00:11:41 +0200

    failed

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,6 +5,16 @@ set -e
 npx lerna run --since --parallel build
 #npx lerna publish prepatch --dist-tag lol --yes --no-push --conventional-commits
 #npx lerna publish patch --preid some-branch  --dist-tag bra1
+
+#npx lerna changed -a
+#if [ "npx lerna changed -a" -eq 0 ]
+#then
+#  echo "Success: I found IP address in file."
+#  exit 0
+#else
+#  echo "Failure: I did not found IP address in file. Script failed" >&2
+#  exit 1
+#fi
 npx lerna publish patch --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
