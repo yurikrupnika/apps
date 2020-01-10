92bfeb1f851a7220b8dc54b308f5d5fbbcd7b224
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 01:09:57 +0200

    major

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -18,11 +18,11 @@ fi
 #cat ~/.npmrc
 #npx lerna exec --parallel --since -- npm i
 npx lerna run --since --parallel build
-npx lerna publish minor --yes --no-push --conventional-commits
+npx lerna publish major --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 git add -u
 git commit -am "package-lock.json update"
-npm version patch
+npm version major
 git push origin --follow-tags
 
 # tag
