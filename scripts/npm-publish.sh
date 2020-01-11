c54f18f030c4b93c7e84e51168189be4d362fd4c
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 23:57:49 +0200

    removed ;erna i[

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -15,25 +15,25 @@ if test -f "$FILE"; then
   echo 'asd'
 #  git reset publish-command.text
   git status
-  npx lerna exec --stream --since -- npm i
-  npx lerna run --parallel build --since
-  npx lerna publish patch --yes --no-push --conventional-commits
-  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-  git status
-#  git checkout -- publish-command.text
-#  git add publish-command.text
-  git status
-  git add -u
-  git commit -am "package-lock.json update"
-  git status
-  npm version patch
-  git push origin --follow-tags
-##
-##  # tag
-##  #$npm_package_version
-#  git pull
-  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
-  git push origin --follow-tags
+#  npx lerna exec --stream --since -- npm i
+#  npx lerna run --parallel build --since
+#  npx lerna publish patch --yes --no-push --conventional-commits
+#  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+#  git status
+##  git checkout -- publish-command.text
+##  git add publish-command.text
+#  git status
+#  git add -u
+#  git commit -am "package-lock.json update"
+#  git status
+#  npm version patch
+#  git push origin --follow-tags
+###
+###  # tag
+###  #$npm_package_version
+##  git pull
+#  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
+#  git push origin --follow-tags
 else
   echo no packages to publish
 fi
