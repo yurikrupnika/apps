8cb3f31a92a92ef0d08593156d927c7f45f07f94
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 03:24:17 +0200

    da

--- scripts/prepare.publish.js
+++ scripts/prepare.publish.js
@@ -14,11 +14,14 @@ async function createFile() {
             (error) => {
                 if (error) {
                     console.log('Failed to write json'); // eslint-disable-line
+                    process.exitCode = 1;
                 } else {
                     console.log('Created private-to-publish.json file'); // eslint-disable-line
+                    process.exitCode = 0;
                 }
             });
     } catch (e) {
+        // console.error('error accured with npm lerna changed');
         process.exitCode = 1;
     }
 }
