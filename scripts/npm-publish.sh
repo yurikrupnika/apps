444f9685d858ec53aa665ac29ee416614413587d
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 07:51:45 +0200

    major

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -30,11 +30,11 @@ if test -f "$FILE"; then
   echo 'asd'
   npx lerna exec --stream --since -- npm i
   npx lerna run --parallel build --since
-  npx lerna publish minor --yes --no-push --conventional-commits
+  npx lerna publish major --yes --no-push --conventional-commits
   npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
   git add -u
   git commit -am "package-lock.json update"
-  npm version minor
+  npm version major
   git push origin --follow-tags
 ##
 ##  # tag
