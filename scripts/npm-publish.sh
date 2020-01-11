c3bc16d2d93f0b45765f7d401e30e0b16833a5c2
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 07:39:25 +0200

    c

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -27,20 +27,20 @@ echo $npm_package_version
 FILE=publish-command.text
 #while read USER; do echo "Hello $USER!"; done < private-to-publish.text
 if test -f "$FILE"; then
+  echo 'asd'
 #  npx lerna exec --stream --since -- npm i
-  npx lerna run --parallel --since build
-  npx lerna publish minor --yes --no-push --conventional-commits
-  npx lerna exec --parallel --since -- npm install --package-lock-only --ignore-scripts --no-audit
-  git add -u
-  git commit -am "package-lock.json update"
-  npm version minor
-  git push origin --follow-tags
-#
-#  # tag
-#  #$npm_package_version
-#  #echo $npm_package_version
-  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
-  git push origin --follow-tags
+#  npx lerna run --parallel --since build
+#  npx lerna publish minor --yes --no-push --conventional-commits
+#  npx lerna exec --parallel --since -- npm install --package-lock-only --ignore-scripts --no-audit
+#  git add -u
+#  git commit -am "package-lock.json update"
+#  npm version minor
+#  git push origin --follow-tags
+##
+##  # tag
+##  #$npm_package_version
+#  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
+#  git push origin --follow-tags
 else
 
   echo no packages to publish
