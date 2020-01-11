dfe70385de3eb785616c25712a36fe75ee9e4252
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 11:02:26 +0200

    tr

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -2,32 +2,11 @@
 
 set -eo pipefail
 echo $npm_package_version
-#echo $(node scripts/prepare.publish.js -q)
-#node scripts/prepare.publish.js
-#npx lerna publish prepatch --dist-tag lol --yes --no-push --conventional-commits
-#npx lerna publish patch --preid some-branch  --dist-tag bra1
 
-#npx lerna changed -a
-
-#node scripts/prepare.publish.js -q
-#file=./private-to-publish.text
-#echo $PATH
-#echo ${cat private-to-publish.text}
-#cat private-to-publish.text
-#while IFS= read -r -uN line
-#do
-#  # do something on $line
-#  echo "$line"
-#done <"$file"
-#echo before cat
-#cat private-to-publish.text
-#echo 'export COMAND2=$(cat private-to-publish.text)'
-#COMAND4=$(cat publish-command.text)
-#echo yse $COMAND4
 
 FILE=publish-command.text
 #while read USER; do echo "Hello $USER!"; done < private-to-publish.text
-if test -f "$FILE"; then
+#if test -f "$FILE"; then
   echo 'asd'
   git status
   npx lerna exec --stream --since -- npm i
@@ -46,7 +25,6 @@ if test -f "$FILE"; then
 #  git pull
   git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
   git push origin --follow-tags
-else
-#
-  echo no packages to publish
-fi
+#else
+#  echo no packages to publish
+#fi
