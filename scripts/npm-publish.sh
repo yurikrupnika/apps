b8df0538805ecf8e14a7c361f41e7fc323135fcb
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 07:05:07 +0200

    commands update

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -10,8 +10,8 @@ echo $npm_package_version
 #npx lerna changed -a
 
 node scripts/prepare.publish.js -q
-file=./private-to-publish.text
-echo $PATH
+#file=./private-to-publish.text
+#echo $PATH
 #echo ${cat private-to-publish.text}
 #cat private-to-publish.text
 #while IFS= read -r -uN line
@@ -19,18 +19,18 @@ echo $PATH
 #  # do something on $line
 #  echo "$line"
 #done <"$file"
-echo before cat
+#echo before cat
 #cat private-to-publish.text
 #echo 'export COMAND2=$(cat private-to-publish.text)'
 #COMAND4=$(cat publish-command.text)
-echo yse $COMAND4
+#echo yse $COMAND4
 FILE=publish-command.text
 #while read USER; do echo "Hello $USER!"; done < private-to-publish.text
 if test -f "$FILE"; then
   npx lerna exec --parallel --since -- npm ci
-  npx lerna run --since --parallel build
+  npx lerna run --parallel --since build
   npx lerna publish minor --yes --no-push --conventional-commits
-  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+  npx lerna exec --parallel --since -- npm install --package-lock-only --ignore-scripts --no-audit
   git add -u
   git commit -am "package-lock.json update"
   npm version minor
