bf786160d4035dfc632ad7a63148970079c81bd4
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-11 06:28:58 +0200

    1

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 set -eo pipefail
+echo $npm_package_version
 #echo $(node scripts/prepare.publish.js -q)
 #node scripts/prepare.publish.js
 #npx lerna publish prepatch --dist-tag lol --yes --no-push --conventional-commits
@@ -9,12 +10,49 @@ set -eo pipefail
 #npx lerna changed -a
 
 node scripts/prepare.publish.js -q
-#PATH=private-to-publish.json
-#echo $PATH
-#cat ./$PATH
-if [ $? -eq 0 ]; then
-  echo "Success: I found IP address in file."
-  #npx lerna exec --parallel --since -- npm ci
+file=./private-to-publish.text
+echo $PATH
+#echo ${cat private-to-publish.text}
+#cat private-to-publish.text
+#while IFS= read -r -uN line
+#do
+#  # do something on $line
+#  echo "$line"
+#done <"$file"
+echo before cat
+#cat private-to-publish.text
+#echo 'export COMAND2=$(cat private-to-publish.text)'
+COMAND4=$(cat publish-command.text)
+echo yse $COMAND4
+#while read USER; do echo "Hello $USER!"; done < private-to-publish.text
+if [ -z "$COMAND4" ]; then
+  echo no packages to publish
+else
+  #if [ $? -eq 0 ]; then
+  #  echo "Success: I found IP address in file."
+  #  #npx lerna exec --parallel --since -- npm ci
+  ##  npx lerna run --since --parallel build
+  ##  npx lerna publish minor --yes --no-push --conventional-commits
+  ##  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+  ##  git add -u
+  ##  git commit -am "package-lock.json update"
+  ##  npm version minor
+  ##  git push origin --follow-tags
+  ##
+  ##  # tag
+  ##  #$npm_package_version
+  ##  #echo $npm_package_version
+  ##  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
+  ##  git push origin --follow-tags
+  ##  node prepare.publish.js
+  #else
+  #  echo "Failure: I did not found IP address in file. Script failed" >&2
+  #  circleci-agent step halt
+  #  exit 0
+  #fi
+  #
+  #cat ~/.npmrc
+  npx lerna exec --parallel --since -- npm ci
   npx lerna run --since --parallel build
   npx lerna publish minor --yes --no-push --conventional-commits
   npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
@@ -28,25 +66,4 @@ if [ $? -eq 0 ]; then
   #echo $npm_package_version
   git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
   git push origin --follow-tags
-#  node prepare.publish.js
-else
-  echo "Failure: I did not found IP address in file. Script failed" >&2
-  circleci-agent step halt
-  exit 0
 fi
-#
-#cat ~/.npmrc
-##npx lerna exec --parallel --since -- npm ci
-##npx lerna run --since --parallel build
-##npx lerna publish minor --yes --no-push --conventional-commits
-##npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-##git add -u
-##git commit -am "package-lock.json update"
-##npm version minor
-##git push origin --follow-tags
-#
-## tag
-##$npm_package_version
-##echo $npm_package_version
-#git tag "$(node -p "require('./package.json').version")"  -a -m "$(node -p "require('./package.json').version")"
-#git push origin --follow-tags
--- scripts/prepare.publish.js
+++ scripts/prepare.publish.js
@@ -2,28 +2,76 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
+const { execFile, spawn } = require('child_process');
 const exec = util.promisify(require('child_process').exec);
+// const child = execFile('node', ['--version'], (error, stdout, stderr) => {
+//     if (error) {
+//         throw error;
+//     }
+//     console.log('stdout', stdout);
+// });
+//
+// child.on('message', (message) => {
+//     console.log('message', message);
+// });
+// child.on('exit', (exit) => {
+//     console.log('exit', exit);
+// });
 
 async function createFile() {
     try {
         const { stdout } = await exec('npx lerna changed -a --json');
-        const modules = JSON.parse(stdout)
-            .filter((m) => m.private);
+        // const modules = JSON.parse(stdout)
+        //     .filter((m) => m.private);
 
-        fs.writeFile(path.join(process.cwd(), 'private-to-publish.json'), JSON.stringify(modules), 'utf8',
-            (error) => {
+        const command = JSON.parse(stdout)
+            .reduce((acc, next, i, list) => {
+                // if (next.private) {
+                    return `${acc || 'npx lerna run'} --scope ${next.name} ${i === list.length - 1 ? 'publish\n' : ''}`;
+                // }
+                // return acc;
+            }, '');
+
+        console.log('command,', command);
+        fs.writeFile(path.join(process.cwd(), 'publish-command.text'),
+            '', 'utf8',
+            async (error) => {
                 if (error) {
                     console.log('Failed to write json'); // eslint-disable-line
                     process.exitCode = 1;
                 } else {
                     console.log('Created private-to-publish.json file'); // eslint-disable-line
+                    // const a = await exec('npx lerna changed -a --json');
+                    // console.log('a', a);
                     process.exitCode = 0;
                 }
             });
     } catch (e) {
         console.error('error accured with npm lerna changed');
-        process.exitCode = 1;
+        process.exitCode = 0;
     }
 }
 
-createFile();
+
+async function readFile() {
+    fs.readFile(path.join(process.cwd(), 'publish-command.text'), 'utf8', (error, d) => {
+        if (error) {
+            console.log('e', error);
+        }
+        console.log('d', d);
+    });
+}
+
+async function build() {
+    console.log('building');
+    const fd = await exec('npx lerna run --since --parallel build', { shell: true });
+    // console.log(fd);
+}
+
+async function control() {
+    await createFile();
+    // await build();
+    // await readFile();
+}
+
+control();
