6765e9a07a7bff882e80af3c0f246a835d4c1cfb
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-12 00:58:28 +0200

    '[sa]'

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -12,31 +12,36 @@ FILE=publish-command.text
 #while read USER; do echo "Hello $USER!"; done < private-to-publish.text
 if ! -f "$FILE"; then
   echo 'asd'
-  git status
-  git rm publish-command.text
+#  git status
+#  git rm publish-command.text
 #git reset --hard
-  echo ${read $FILE}
+#  echo ${'cat publish-command.text}
 #  rm publish-command.text
+  git add publish-command.text .
+  cat publish-command.text
   git status
-#  npx lerna exec --stream --since -- npm i
-#  npx lerna run --parallel build --since
-#  npx lerna publish patch --yes --no-push --conventional-commits
-#  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-#  git status
-##  git checkout -- publish-command.text
-##  git add publish-command.text
-#  git status
-#  git add -u
-#  git commit -am "package-lock.json update"
-#  git status
-#  npm version patch
-#  git push origin --follow-tags
+#  rm publish-command.text
+#  git rm publish-command.text .
+  git status
+  npx lerna exec --stream --since -- npm i
+  npx lerna run --parallel build --since
+  npx lerna publish patch --yes --no-push --conventional-commits
+  npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+  git status
+#  git checkout -- publish-command.text
+#  git add publish-command.text
+  git status
+  git add -u
+  git commit -am "package-lock.json update"
+  git status
+  npm version patch
+  git push origin --follow-tags
 ###
 ###  # tag
 ###  #$npm_package_version
 ##  git pull
-#  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
-#  git push origin --follow-tags
+  git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
+  git push origin --follow-tags
 else
   echo no packages to publish
 fi
