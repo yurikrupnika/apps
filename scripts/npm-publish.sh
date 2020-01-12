ee046543356f5c62d8a5aa5815c977b93c654b6b
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-12 06:28:03 +0200

    major

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -33,19 +33,17 @@ cat publish-command.text
 #  git status
 npx lerna exec --stream --since -- npm i
 npx lerna run --parallel build --since
-npx lerna publish patch --yes --no-push --conventional-commits
+npx lerna publish major --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 #  git status
 #  git checkout -- publish-command.text
 #  git add publish-command.text
 #  git status
-npm version patch
-git push origin --follow-tags
-
 git add -u
 git commit -am "package-lock.json update"
 #  git status
-
+npm version major
+git push origin --follow-tags
 ###
 ###  # tag
 ###  #$npm_package_version
