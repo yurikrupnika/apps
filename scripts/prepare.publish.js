69fb36b004d4171ca0b70ce5588456dfe6d902e4
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-12 03:16:24 +0200

    20

--- scripts/prepare.publish.js
+++ scripts/prepare.publish.js
@@ -9,7 +9,7 @@ async function createFile() {
         const { stdout } = await exec('npx lerna changed -a --json');
         const command = JSON.parse(stdout)
             .reduce((acc, next, i, list) => {
-                return `${acc || 'npx lerna run'} --scope ${next.name} ${i === list.length - 1 ? 'deploy\n' : ''}`;
+                return `${acc || 'npx lerna run'} --scope ${next.name} ${i === list.length - 1 ? 'deploy' : ''}`;
             }, '');
 
         console.log('command', command);
@@ -27,20 +27,20 @@ async function createFile() {
     } catch (e) {
         console.error('error accured with npm lerna changed');
         // console.log('writing empty file');
-        fs.access(path.join(process.cwd(), 'publish-command.text'), fs.F_OK, (error, a) => {
-            if (error) {
-                console.log('no file found');
-            } else {
-                console.log('file found and will be removed');
-                fs.unlink(path.join(process.cwd(), 'publish-command.text'), (err) => {
-                    if (err) {
-                        console.log('failed to removed file');
-                    } else {
-                        console.log('removed file succefully');
-                    }
-                });
-            }
-        });
+        // fs.access(path.join(process.cwd(), 'publish-command.text'), fs.F_OK, (error, a) => {
+        //     if (error) {
+        //         console.log('no file found');
+        //     } else {
+        //         console.log('file found and will be removed');
+        //         fs.unlink(path.join(process.cwd(), 'publish-command.text'), (err) => {
+        //             if (err) {
+        //                 console.log('failed to removed file');
+        //             } else {
+        //                 console.log('removed file succefully');
+        //             }
+        //         });
+        //     }
+        // });
         // fs.writeFile(path.join(process.cwd(), 'publish-command.text'),
         //     '', 'utf8',
         //     async (error) => {
@@ -75,6 +75,17 @@ async function build() {
 async function control() {
     await createFile();
     // await build();
+    fs.writeFile(path.join(process.cwd(), 'publish-command.text'),
+        '', 'utf8',
+        async (error) => {
+            if (error) {
+                console.log('Failed to write json'); // eslint-disable-line
+                process.exitCode = 1;
+            } else {
+                console.log('Created private-to-publish.json file'); // eslint-disable-line
+                process.exitCode = 0;
+            }
+        });
     // await readFile();
 }
 
