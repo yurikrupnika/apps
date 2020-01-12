a33eec8583bf87514d255ca0841f3765e0eef1d1
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-12 06:20:37 +0200

    123

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -39,11 +39,13 @@ npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
 #  git checkout -- publish-command.text
 #  git add publish-command.text
 #  git status
+npm version patch
+git push origin --follow-tags
+
 git add -u
 git commit -am "package-lock.json update"
 #  git status
-npm version patch
-git push origin --follow-tags
+
 ###
 ###  # tag
 ###  #$npm_package_version
