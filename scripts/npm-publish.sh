2b8adb51761528e62fc4c3dd468b734cafbc9fb5
yurikrupnik <krupnik.yuri@gmail.com>
2020-01-12 15:09:31 +0200

    statuses

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -35,10 +35,10 @@ npx lerna exec --stream --since -- npm i
 npx lerna run --parallel build --since
 npx lerna publish major --yes --no-push --conventional-commits
 npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
-#  git status
+  git status
 #  git checkout -- publish-command.text
-#  git add publish-command.text
-#  git status
+  git add publish-command.text
+  git status
 git add -u
 git commit -am "package-lock.json update"
 git status
@@ -47,7 +47,7 @@ git push origin --follow-tags
 ###
 ###  # tag
 ###  #$npm_package_version
-git status
+  git status
 git tag "$(node -p "require('./package.json').version")" -a -m "$(node -p "require('./package.json').version")"
 git push origin --follow-tags
 #else
